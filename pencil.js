210a5f67b76915af32fb14d070f402df6cf34440
kripakris <98467708+kripakris@users.noreply.github.com>
2022-10-24 18:44:02 +0530

    Add files via upload

---
+++ pencil.js
@@ -0,0 +1,30 @@
+img = "";
+status ="";
+object = [];
+function preload(){
+    img = loadImage("pencil.jpg")
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting objects";
+}
+
+function modelLoaded() {
+    console.log("model loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+
+function gotResult(error , results){
+if (error) {
+    console.log(error);
+}
+console.log(results);
+
+objects = results;
+}
